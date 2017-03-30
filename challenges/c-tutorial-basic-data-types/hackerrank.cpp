d3eb05fb44198be9eb769624f40d432e696a512f
Tom Lee <jungdal@empas.com>
2017-03-30 17:50:20 +0900

    solved c-tutorial-basic-data-types

---
+++ challenges/c-tutorial-basic-data-types/hackerrank.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    // start
+    int a;
+    long b;
+    long long c;
+    char d;
+    float e;
+    double f;
+    scanf("%d %ld %lld %c %f %lf", &a, &b, &c, &d, &e, &f);
+    printf("%d\n%ld\n%lld\n%c\n%.3f\n%.9lf\n", a, b, c, d, e, f);
+    // end
+    return 0;
+}
